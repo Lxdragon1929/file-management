6f2834a0414c1470c89d5f7383213f2a2ecd953e
lixin <lx@redrcd.com>
2021-03-26 16:53:39 +0800

    修改添加文件方法

--- src/main/java/com/home/fileManagement/servie/impl/FileServiceImpl.java
+++ src/main/java/com/home/fileManagement/servie/impl/FileServiceImpl.java
@@ -26,6 +26,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -50,6 +51,10 @@ public class FileServiceImpl implements FileService {
     @Override
     public boolean add(User user, MultipartFile[] files) {
         try{
+            File fileDir = new File(basePath+"\\"+user.getUserName());
+            if(!fileDir.exists()) {
+                fileDir.mkdirs();
+            }
             for(int i=0;i<files.length;i++){
                 uploadAndAdd(user, files[i]);
             }
